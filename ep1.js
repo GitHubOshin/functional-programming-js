f3c176b59c46918f303c3fdf468aab9fa760b939
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2023-01-29 23:46:59 +0700

    Update ep1.js

--- ep1.js
+++ ep1.js
@@ -1,1 +1,52 @@
+// 1. Pure Function
 
+function addPureFn(a, b) {
+  return a + b
+}
+
+console.log('Pure Function :', addPureFn(1, 2))
+
+// 2. Avoid Side Effect
+
+const b = 6
+function addASF(p) {
+  return p + b
+}
+
+console.log('Avoid Side Effect :', addASF(1))
+
+/**
+ * *3. First class function
+ *
+ *   *3.1 Assigning a function to a variable*/
+
+function addAssigning(a, b) {
+  return a + b
+}
+
+console.log('Assigning :', addAssigning(1, 6))
+
+/**
+ *    *3.2 Returning a function*/
+
+function addReturning(a, b) {
+  return function () {
+    return a + b
+  }
+}
+
+const addTwoNumber = addReturning(1, 6)
+console.log('Returning :', addTwoNumber())
+
+/**
+ *   *Accepting a function as an argument*/
+
+function addNumber(a, b) {
+  return a + b
+}
+
+function addAccepting(add, a, b) {
+  return add(a + b)
+}
+
+console.log('Accepting : ', addAccepting(addNumber, 1, 6))
