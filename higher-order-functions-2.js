83dd69e6db62843fca5519c26932eb19580be856
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2023-01-31 00:22:06 +0700

    Update higher-order-functions-2.js
    forEach / find, findIndex / every, some

--- higher-order-functions-2.js
+++ higher-order-functions-2.js
@@ -1,1 +1,64 @@
+/**
+ * * EP 3*/
 
+const persons = [
+  { name: 'Jane', age: 9 },
+  { name: 'Jane', age: 10 },
+  { name: 'Jim', age: 15 },
+  { name: 'John', age: 17 },
+  { name: 'Joey', age: 20 }
+]
+
+/**
+ * * forEach*/
+
+// for (let i = 0; i < persons.length; i++) {
+//   const person = persons[i]
+//   // if (person.age > 15) continue
+//   console.log(`Name ${person.name}, Age ${person.age}`)
+// }
+
+persons.forEach((person) =>
+  console.log(`Name ${person.name}, Age ${person.age}`)
+)
+
+/**
+ * * find, findIndex*/
+// let jane
+// for (let i = 0; i < persons.length; i++) {
+//   const person = persons[i]
+//   if (person.name === 'Jane') {
+//     jane = person
+//     break
+//   }
+// }
+
+const jane = persons.find((person) => person.name === 'Jane')
+
+console.log('Jane :', jane)
+
+const janeIndex = persons.findIndex((person) => person.name === 'Jane')
+
+console.log('Jane Index :', janeIndex)
+
+/**
+ * * every, some */
+
+// let isKid = true
+
+// for (let i = 0; i < persons.length; i++) {
+//   const person = persons[i]
+//   if (person.age > 15) {
+//     // console.log(person.age)
+//     isKid = false
+//     break
+//   }
+// }
+
+const isKidEvery = persons.every((person) => person.age < 15)
+
+console.log('isKid : ', isKidEvery)
+
+const isKidSome = persons.some((person) => person.age < 15)
+
+console.log('isKid : ', isKidSome)
